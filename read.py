877466889cdf46c86c4ad094a6d7e900b52bc53a
fishhuman <fish.human10@gmail.com>
2019-06-25 20:13:12 +0800

    add good filter

--- read.py
+++ read.py
@@ -17,5 +17,12 @@ new = []
 for d in data:
     if len(d) < 100:
         new.append(d)
-print('一共有', len(new), '筆留言長度小於100')            
+print('一共有', len(new), '筆留言長度小於100')
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d) 
+print('一共有', len(good), '筆留言提到good')
+print[0]                   
     
